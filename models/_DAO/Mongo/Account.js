176e6189651ef7d439f042c9905598a955c01daa
jinok <wlsdhrvmfh@naver.com>
2020-10-03 16:31:13 +0900

    db conn 분리, schema 분리

--- models/_DAO/Mongo/Account.js
+++ models/_DAO/Mongo/Account.js
@@ -1,4 +1,4 @@
 // Account.js
 const mongoose = require('mongoose');
-const conn = mongoose.createConnection('mongodb://localhost:27017' + '/Account');
+const conn = mongoose.createConnection(process.env.DB_CONNECTION_URI_MONGO + '/Data');
 module.exports = conn;
--- models/_DAO/Mongo/Log.js
+++
@@ -1,4 +0,0 @@
-// Log.js
-const mongoose = require('mongoose');
-const conn = mongoose.createConnection(process.env.LOG_DB_CONNECTION_URI_MONGO + '/Log');
-module.exports = conn;
